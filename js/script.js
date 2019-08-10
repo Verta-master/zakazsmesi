95383e5b9d1f35623b28713ad1b53cbe48b451eb
Verta-master <vertyagina@gmail.com>
2019-08-10 21:40:22 +0600

    Update 2019-08-10T15:40:16.190Z

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-$(".header__button").click(function(e){e.preventDefault,e.stopPropagation()}),window.innerWidth<1022&&($(".header__button").click(function(e){e.preventDefault,e.stopPropagation(),$(".sidebar").toggleClass("sidebar--open")}),$("body").click(function(){$(".sidebar").removeClass("sidebar--open")}));
+$('.header__button').click(function(evt) {
+  evt.preventDefault;
+  evt.stopPropagation();
+});
+
+if (window.innerWidth < 1022) {
+  $('.header__button').click(function(evt) {
+    evt.preventDefault;
+    evt.stopPropagation();
+    $('.sidebar').toggleClass('sidebar--open');
+  });
+  
+  $('body').click(function(){
+    $('.sidebar').removeClass('sidebar--open');
+  });
+}
